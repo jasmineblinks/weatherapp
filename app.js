c9791c08630b09a87927bf0bd518164de57bd6f4
Faith K Pueneh <puenehfaith@gmail.com>
2023-08-23 17:13:05 +0100

    updated app.js

--- app.js
+++ app.js
@@ -1,9 +1,23 @@
 const apiKey="f7027645d2bba031b997ec1855c37aa0"
-const apiUrl="https://api.openweathermap.org/data/2.5/weather? units=metric&q=bangalore"
-async function checkWeather(){
-    const response =await fetch(apiUrl +`&appid=${apiKey}` )
+const apiUrl="https://api.openweathermap.org/data/2.5/weather?units=metric&q="
+
+const searchbox= document.querySelector('.search input')
+
+const searchbtn= document.querySelector('.search button')
+
+
+
+async function checkWeather(city){
+    const response =await fetch(apiUrl+city +`&appid=${apiKey}` )
     var data =await response.json();
     console.log(data)
-    document.querySelector(".city").innerHTML= data.name
+    document.querySelector(".city").innerHTML= data.name;
+    document.querySelector(".temp").innerHTML= Math.round(data.main.temp) + '°C';
+    document.querySelector(".humidty").innerHTML= data.main.humidity+'%';
+    document.querySelector(".wind").innerHTML= data.wind.speed + "km/hr";
 }
+searchbtn.addEventListener("click", ()=>{
+    checkWeather(searchbox.value)
+
+})
 checkWeather()
